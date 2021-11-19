f71365e3ee5cebb683399acd4e166e2caccb4fe0
Ash Blue <ash@blueashes.com>
2021-11-18 20:32:52 -0700

    Updated resume link to new Dropbox PDF

--- src/components/organisms/intro/Resume.vue
+++ src/components/organisms/intro/Resume.vue
@@ -4,18 +4,11 @@
 
     <a
       target="_blank"
-      href="https://docs.google.com/document/d/1gp4HYCqg0lmjAu0Kec9uRS9y_Z5gCf7UGIV7YKf0UIo/export?format=pdf&id=1gp4HYCqg0lmjAu0Kec9uRS9y_Z5gCf7UGIV7YKf0UIo&token=AC4w5ViGjzNzp-tkNg_qW9n_PJP415kNFQ%3A1366077437776"
+      href="https://www.dropbox.com/s/g4qs3w85qqldu1s/Resume-Ash-Blue.pdf?dl=0"
       class="btn btn-primary btn-lg btn-block mb-2">
       <Icon namespace="fas" icon="file-pdf" />&nbsp;
-      Download Resume
+      View Resume
     </a>
-
-    <p class="small text-muted text-center">
-      Resume available in multiple formats via
-      <a
-        target="_blank"
-        href="https://docs.google.com/document/d/1gp4HYCqg0lmjAu0Kec9uRS9y_Z5gCf7UGIV7YKf0UIo/edit?usp=sharing">Google Drive <Icon icon="google-drive" /></a>
-    </p>
   </div>
 </template>
 
